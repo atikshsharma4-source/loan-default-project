b88f595c7ea9d0f92e3d51c55a9155904008079f
Atiksh Sharma <atikshsharma4@gmail.com>
2026-08-15 18:50:02 +0530

    Allow deployed frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -11,9 +11,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://loan-default-project-xg3b.onrender.com"
     ],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
