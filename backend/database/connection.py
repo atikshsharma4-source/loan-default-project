dc4988b3bb04076b5b23da4ff77e516fb53a33ce
Atiksh Sharma <atikshsharma4@gmail.com>
2026-08-15 18:03:40 +0530

    Connect backend to cloud database

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,9 +1,16 @@
 import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
 connection = mysql.connector.connect(
-    host=os.getenv("DB_HOST", "localhost"),
-    user=os.getenv("DB_USER", "root"),
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME", "loan_risk_db")
+    database=os.getenv("DB_NAME"),
+    ssl_disabled=False
 )
+
 print("MySQL connected successfully!")
